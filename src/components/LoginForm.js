938eb35e5de64a7a3488e1ebf3894616f9c7ba6e
Kush Patel <kushpatel@asite.com>
2018-12-15 13:09:40 +0530

    Email Address Input Text Ready[F

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
-import { View, Text} from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
 
+state = { text: '' };
   render() {
     return (
       <Card>
+        <CardSection>
+          <Input
+            hint="user@gmail.com"
+            label="Email"
+            value={ this.state.text }
+            onChangeText = { text => this.setState({ text })}
+             />
+        </CardSection>
+
+        <CardSection/>
         <CardSection>
           <Button>
             Log In
